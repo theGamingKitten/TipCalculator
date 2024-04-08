088eeedc3c61d824413e78aac084469940c207f7
theGamingKitten <56452901+theGamingKitten@users.noreply.github.com>
2024-04-08 22:02:17 +0200

    static site done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import logo from '../public//img/logo.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className="logos">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://tailwindcss.com/" target='_blank'>          
-        <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/320px-Tailwind_CSS_Logo.svg.png" />          
-        </a>
+      <main>
+      <img className='logo' src={logo}/>
+      <div id='container' className='container'>
+      {/*left side*/}
+        <div id='form-container' className='form-container'>
+          <span className='form-label'>Bill</span>
+          <input type='text' placeholder='0' id='bill'></input>
+          <span className='form-label'>Select Tip %</span>
+          <div className='button-container'>
+            <button>5%</button>
+            <button>10%</button>
+            <button>15%</button>
+            <button>25%</button>
+            <button>50%</button>
+            <input type='text' className='custom-input' placeholder='Custom'></input>
+          </div>
+          <span className='form-label'>Number of People</span>
+          <input type='text' placeholder='0' id='person'></input>
+        </div>
+      {/*right side*/}
+        <div id='output-container' className='output-container'>
+          <div className='output-values-container'>
+            <span className='output-label'>Tip Amount 
+            <span className='per-person-label'>/ person</span></span>
+            <span className='output-value'>$0.00</span>
+            <span className='output-label'>Total 
+            <span className='per-person-label'>/ person</span></span>
+            <span className='output-value'>$0.00</span>
+          </div>
+          <button className='reset-btn'>RESET</button>
+        </div>
       </div>
-      <h1>Vite + React + TailwindCSS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the logos to learn more
-      </p>
+      </main>
     </>
   )
 }
